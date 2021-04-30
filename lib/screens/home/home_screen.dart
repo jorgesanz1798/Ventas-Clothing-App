c5ac41905ab7e4eeccd905c4e4138716a82703d0
A814978 <jorge.sanz.external@atos.net>
2021-04-30 18:50:03 +0200

    Limpieza de la app estandar y conectar firebase

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-//import 'package:firebase_storage/firebase_storage.dart'
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.white, elevation: 0),
+      body: Text('Ventas Clothing'),
     );
   }
 }
