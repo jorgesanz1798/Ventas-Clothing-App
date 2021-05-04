aef336503134e84906c66e495811612e0fb31643
A814978 <jorge.sanz.external@atos.net>
2021-05-04 16:05:25 +0200

    Encabezado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ventasclothing/constants.dart';
-import 'package:ventasclothing/screens/home/home.dart';
+import 'package:ventasclothing/screens/cart/cart_screen.dart';
+import 'package:ventasclothing/screens/categories/categories_screen.dart';
+import 'package:ventasclothing/screens/home/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,6 +20,67 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    CategoriesScreen(),
+    CartScreen(),
+    Text('Profile')
+  ];
+
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 1,
+        toolbarHeight: 0,
+      ),
+      body: Center(
+        child: SafeArea(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.black),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.apps, color: Colors.black),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart, color: Colors.black),
+            label: ('Cart'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle, color: Colors.black),
+            label: 'Profile',
+          )
+        ],
+        fixedColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTap,
+        elevation: 0,
+      ),
     );
   }
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen23 extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Home"),
+    return Container(
+      margin: EdgeInsets.only(top: 25, left: 14, right: 14),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.network(
+                'https://firebasestorage.googleapis.com/v0/b/ventas-clothing-app.appspot.com/o/Logo%20VC%20original.png?alt=media&token=f984d718-fd48-4049-ae57-29784da55269',
+                scale: 10.0,
+              ),
+              Text(
+                'Ventas \n\n Clothing',
+                style: TextStyle(fontSize: 20, height: 0.5),
+                textAlign: TextAlign.center,
+              ),
+              IconButton(
+                  onPressed: null,
+                  icon: Icon(Icons.search, color: Colors.black))
+            ],
+          ),
+        ],
       ),
     );
   }
