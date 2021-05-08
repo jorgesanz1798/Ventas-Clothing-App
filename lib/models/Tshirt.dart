2cb3852d94ae0c02541f8c267c4915072d564663
A814978 <jorge.sanz.external@atos.net>
2021-05-08 18:25:51 +0200

    Intentando mostrar camisetas

--- lib/models/Tshirt.dart
+++ lib/models/Tshirt.dart
@@ -1,13 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Tshirt {
-  final String image, title, description;
-  final int price, size, id;
+class Tshirt extends StatelessWidget {
+  /*final String id, image, title, description;
+  int size;
+  int price, stock;
   final Color color;
+  bool favourite;
 
   //Constructor
-  Tshirt(this.image, this.title, this.description, this.price, this.size,
-      this.id, this.color);
+  Tshirt.items(this.image, this.title, this.description, this.price, this.size,
+      this.id, this.color, this.stock, this.favourite);
+  */
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: StreamBuilder(
+          stream:
+              FirebaseFirestore.instance.collection("camisetas").snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            List<DocumentSnapshot> documents = snapshot.data!.docs;
+            print(documents);
+            return ListView.builder(
+                itemCount: documents.length,
+                itemBuilder: (context, index) {
+                  Map<String, dynamic> data =
+                      documents[index].data as Map<String, dynamic>;
+                  return Container(
+                    child: Image(
+                      image: data['Image'],
+                    ),
+                  );
+                });
+          },
+        ),
+      ),
+    );
+  }
 }
-
-List<Tshirt> tshirts = [];
--- lib/screens/categories/categories_screen.dart
+++ lib/screens/categories/categories_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants.dart';
@@ -43,6 +44,32 @@ class CategoriesScreen extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+            StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection("camisetas")
+                  .snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                List<DocumentSnapshot> documents = snapshot.data!.docs;
+                print(documents);
+                return ListView.builder(
+                  itemCount: documents.length,
+                  itemBuilder: (context, index) {
+                    Map<String, dynamic> data =
+                        documents[index].data as Map<String, dynamic>;
+                    return Container(
+                      child: Text(
+                        data['Image'],
+                      ),
+                    );
+                  },
+                );
+              },
             ),
           ],
         ),
